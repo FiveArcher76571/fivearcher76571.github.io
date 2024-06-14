c23fcf22de9354fdd93a759d88d0437c54f062da
Saicharan Vemuri <vemuri.saicharan@gmail.com>
2024-06-13 23:50:14 -0400

    Make updates and try to fix back to top

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,3 +1,16 @@
 var button = document.getElementById("totop");
 
+window.onscroll = function() {whenscroll()};
+
+function whenscroll() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.classList.add("show");
+    }
+    else {
+        button.classList.remove("show");
+    }
+
+}
+
 document.querySelector("#totop").addEventListener("click", function() {window.scroll(0, 0);});
